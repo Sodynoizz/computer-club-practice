63036eeeb5481acc5609d78732614174250143bd
Sodynoizz_TH <tepza5556789@gmail.com>
2023-06-20 00:24:36 +0700

    update

--- src/routes/first-day/+page.svelte
+++ src/routes/first-day/+page.svelte
@@ -2,7 +2,7 @@
 <section>
     <h2>Cat Photo</h2>
     <p>See more <a href="https://unsplash.com/images/animals/cat">cat</a> photo in our gallery</p>
-    <img src="src/img/cat.png" alt="This is a cat"/>
+    <img src="src/images/cat.png" alt="This is a cat"/>
 </section>
 
 
