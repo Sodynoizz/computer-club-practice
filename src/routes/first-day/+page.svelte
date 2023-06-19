c219d1d145a4846eacac684ed3e519996bc13113
Sodynoizz_TH <tepza5556789@gmail.com>
2023-06-20 00:32:29 +0700

    fix cat pic

--- src/routes/first-day/+page.svelte
+++ src/routes/first-day/+page.svelte
@@ -2,7 +2,7 @@
 <section>
     <h2>Cat Photo</h2>
     <p>See more <a href="https://unsplash.com/images/animals/cat">cat</a> photo in our gallery</p>
-    <img src="src/images/cat.png" alt="This is a cat"/>
+    <img src="https://images.wallpaperscraft.com/image/single/cat_surprise_look_96597_3840x2160.jpg" alt="This is a cat"/>
 </section>
 
 
