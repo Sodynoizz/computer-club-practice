a18c8364cb13466d1abee27fdb91b2cab740eb28
Sodynoizz <tepza5556789@gmail.com>
2023-07-03 16:12:13 +0700

    Reformat code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,42 @@
+<script>
+  const navlinks = [{ name: "Practice", path: "/practice" }];
+</script>
+
 <svelte:head>
   <style>
-    @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap");
     body {
-        font-family: 'Kanit', sans-serif;
+      font-family: "Kanit", sans-serif;
     }
   </style>
 </svelte:head>
 
-<script>
-    const navlinks = [
-        { name: "Practice", path: "/practice"},
-    ];
-</script>
-
 <div id="container">
-    <h1>Computer Club Triamudom Launchpad</h1>
-    <h2>All routes</h2>
-    <nav>
-        { #each navlinks as link }
-            <div class="routes">
-                <li><a href="{link.path}">{link.name}</a></li>
-            </div>
-        { /each }
-    </nav>
+  <h1>Computer Club Triamudom Launchpad</h1>
+  <h2>All routes</h2>
+  <nav>
+    {#each navlinks as link}
+      <div class="routes">
+        <li><a href={link.path}>{link.name}</a></li>
+      </div>
+    {/each}
+  </nav>
 </div>
 
 <style>
-    h1 {
-        text-align: center;
-    }
-    
-    h2 {
-        text-align: center;
-    }
+  h1 {
+    text-align: center;
+  }
 
-    .routes {
-        text-align: center;
-    }
+  h2 {
+    text-align: center;
+  }
 
-    .routes a {
-        text-decoration: none;
-    }
+  .routes {
+    text-align: center;
+  }
 
+  .routes a {
+    text-decoration: none;
+  }
 </style>
